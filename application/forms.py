0d0e43e0a6e5a3ddbed7df1eb2909619846c1e00
Ubuntu <ubuntu@ip-172-31-25-17.eu-west-1.compute.internal>
2021-09-12 00:33:10 +0000

    updated forms file

--- application/forms.py
+++ application/forms.py
@@ -2,17 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
-class ListForm(FlaskForm):
-    list_name = StringField("List Name", validators= [DataRequired()])
+class itemForm(FlaskForm):
+    itemName = StringField("Item Name", validators= [DataRequired()])
+    itemColour = StringField("Item Colour", validators= [DataRequired()])
     submit = SubmitField("Submit")
 
-class ItemForm(FlaskForm):
-    name = StringField("Item Name", validators= [DataRequired()])
-    quantity = IntegerField("Item Quantity", default=1)
+class manForm(FlaskForm):
+    manName = StringField("Manufacturer Name", validators= [DataRequired()])
+    manSpec = StringField("Manufacturer Specialty", validators= [DataRequired()])
     submit = SubmitField("Add item")
-
-
-# class ItemLinkForm(FlaskForm):
-#     item = IntegerField("Item ID")
-#     itemList = IntegerField("ItemList ID")
-#     submit = SubmitField("Add item to list")
