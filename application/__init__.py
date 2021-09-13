74faabbc80e40b8a62b7ac8ffa78d82fcbca194b
Ubuntu <ubuntu@ip-172-31-25-17.eu-west-1.compute.internal>
2021-09-13 02:07:15 +0000

    added hyperlinks to readme

--- application/__init__.py
+++ application/__init__.py
@@ -4,9 +4,8 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# getenv('DATABASE_URI'
-app.config['SECRET_KEY'] = "my-secret-key"
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = getenv('my-secret-key')
 
 db = SQLAlchemy(app)
 
